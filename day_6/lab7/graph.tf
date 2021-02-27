21db9e527b1eeff1c5d8618e4aa74f4d094a2a3a
cloudlogic20 <cloudlogic20@gmail.com>
2021-02-27 11:10:26 +0530

    added day_6

---
+++ day_6/lab7/graph.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_version = "0.14.6"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "=3.27.0"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+resource "aws_instance" "myec2" {
+  ami           = "ami-08f63db601b82ff5f"
+  instance_type = "t2.micro"
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.myec2.id
+  vpc      = true
+}
+
+resource "aws_security_group" "allow_tls" {
+  name = "allow_tls"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_eip.lb.private_ip}/32"]
+
+  }
+}
